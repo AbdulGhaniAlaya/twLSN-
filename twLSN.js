075f1f33cf3ae188054028083473d0c76d3b65e9
AbdulGhaniAlaya <76837885+AbdulGhaniAlaya@users.noreply.github.com>
2023-11-07 21:29:21 +0300

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -2,7 +2,7 @@ var twLSN = [
 'tokowhatsapp.blogspot.com',
 'toko-whatsapp.blogspot.com',
 'jasa-design.web.id',
-'books-sy.blogspot.com',
+'demo-abdalane2019.blogspot.com',
 ];
 
 var redirectUrl = 'https://arab-content.blogspot.com';
